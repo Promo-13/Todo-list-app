29cdd44b0f53baf5133162927b1cc7ce416e6556
Promodh <promodh2003@gmail.com>
2023-04-01 00:11:04 +0530

    #if-name-equal-main

--- function.py
+++ function.py
@@ -12,3 +12,10 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do list items in the text file"""
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
+
+
+"""
+This will be executed only when this function is executed and not when it is imported
+"""
+if __name__ == "__main__":
+    print("Hello")
